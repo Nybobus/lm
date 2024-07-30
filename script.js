5f4883a5525584a9765c6af1309ff9e80eeeb338
Nybobus <150648614+Nybobus@users.noreply.github.com>
2024-07-30 11:20:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// for (let i = 20; i > 10; i--) {
+//     console.log(i);
+// }
+
+// let i = 0
+// while(i < 10) {
+//     console.log(i);
+//     i++
+// }
+
+// let i = 0
+// do {
+//     console.log(`hello`);
+//     i++
+// } while (1 < 5)
+let sum = 0
+for (let i = 0; i <= 10; i++) {
+    sum = sum + 1
+}
+console.log(sum);
